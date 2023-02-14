a0cbf9e95a8d6737dc67a358747a5d7f7f116696
pocketclint <agileclint@gmail.com>
2023-02-14 15:19:45 -0600

    test suite created

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,1 +1,19 @@
 const Engineer = require("../lib/Engineer");
+
+test("Can set GitHUb account via constructor", () => {
+    const testValue = "GitHubUser";
+    const e = new Engineer("foo", 1, "fake@email.com", testValue);
+    expect(e.github).toBe(testValue);
+});
+
+test("getRole() should return \"Engineer\"", () => {
+    const testValue = "Engineer";
+    const e = new Engineer("foo", 1, "fake@email.com", "GitHubUser");
+    expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get GitHub username via getGithub()", () => {
+    const testValue = "GitHubUser";
+    const e = new Engineer("foo", 1, "fake@email.com", testValue);
+    expect(e.getGithub()).toBe(testValue);
+});
